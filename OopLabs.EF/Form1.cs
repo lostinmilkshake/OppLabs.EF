37ecc52626be1d95fc217cc9e42f8a1a59cf559e
Kirill Litvinov <kirillitvinov@gmail.com>
2021-11-09 21:29:58 +0300

    Добавьте файлы проекта.

---
+++ OopLabs.EF/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OopLabs.EF
+{
+    public partial class Form1 : Form
+    {
+        private readonly AdventureWorksEntities _entities = new AdventureWorksEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
